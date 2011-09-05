7a1356c589d1b816b2a5e19e23c01f0f880b53ed
congchenutd <congchenutd@14d3f5f5-9e0c-e853-d946-f1305e45a466>
2011-09-05 23:08:11 +0000

    + phase divider

--- trunk/PhaseDivider.cpp
+++ trunk/PhaseDivider.cpp
@@ -1,11 +1,54 @@
 #include "PhaseDivider.h"
+#include <QStringList>
 
 PhaseDivider::PhaseDivider(const Events& events, int f)
-	: fuzziness(f)//, originalEvents(events)
+	: fuzziness(f), originalEvents(events)
 {
+	qSort(originalEvents);   // sort by time
 }
 
-void PhaseDivider::addPhase(const QString& phase)
+Events PhaseDivider::addPhase(const QString& phase) const
 {
+	Events result;
 
+	// get all events in this phase
+	Events events;
+	double sum = 0.0;
+	QDateTime start;
+	foreach(TeamRadarEvent event, originalEvents)
+		if(event.getPhase() == phase)
+		{
+			if(events.isEmpty())  // first event, the start
+				start = event.time;
+			else
+				sum += start.secsTo(event.time);  // sum up
+
+			events << event;
+		}
+	if(events.isEmpty())
+		return result;
+
+	// get the center, max radius, and radius
+	double average = sum / events.size();
+	QDateTime center = start.addSecs(average);
+	long distanceStart = start.secsTo(center);
+	long distanceEnd   = center.secsTo(events.at(events.size() - 1).time);
+	long maxRadius = qMax(distanceStart, distanceEnd);
+	long radius = fuzziness * maxRadius / 100;
+
+	// produce the cluster
+	foreach(TeamRadarEvent event, originalEvents)
+		if(event.time < center && event.time.secsTo(center) < radius)
+			result << event;
+		else if(event.time >= center && center.secsTo(event.time) <= radius)
+			result << event;
+	return result;
+}
+
+Events PhaseDivider::getEvents(const QStringList& phases) const
+{
+	Events result;
+	foreach(QString phase, phases)
+		result << addPhase(phase);
+	return result;
 }
